9feb105e28008c5c3996ce3c2eabade2b6f80754
정지원 <poiu8944@gmail.com>
2020-06-07 18:05:38 +0900

    201502119 정지원 실전코딩 과제 제출

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -5,6 +5,7 @@ import Panel from "./Panel";
 import Display from "./Display";
 import ButtonGroup from "./ButtonGroup";
 import Button from "./Button";
+import History from "./History";
 
 const Container = styled.div`
   margin: 30px auto;
@@ -30,17 +31,20 @@ const Box = styled.div`
 
 const evalFunc = function(string) {
   // eslint-disable-next-line no-new-func
+  if (string.indexOf("÷") !== -1) string = string.replace(/÷/g, "/");
+  if (string.indexOf("×") !== -1) string = string.replace(/×/g, "*");
   return new Function("return (" + string + ")")();
 };
 
 class Calculator extends React.Component {
   // TODO: history 추가
   state = {
-    displayValue: ""
+    displayValue: "",
+    history: [],
   };
 
-  onClickButton = key => {
-    let { displayValue = "" } = this.state;
+  onClickButton = (key) => {
+    let { displayValue = "", history = [] } = this.state;
     displayValue = "" + displayValue;
     const lastChar = displayValue.substr(displayValue.length - 1);
     const operatorKeys = ["÷", "×", "-", "+"];
@@ -55,11 +59,34 @@ class Calculator extends React.Component {
         this.setState({ displayValue });
       },
       // TODO: 제곱근 구현
-      "√": () => {},
+      "√": () => {
+        if (lastChar !== "" && !operatorKeys.includes(lastChar)) {
+          let prevVal = "√(" + displayValue + ")";
+          displayValue = evalFunc(displayValue);
+          this.setState({
+            history: history.concat({
+              prevVal,
+              displayValue: Math.sqrt(displayValue),
+            }),
+          });
+        }
+      },
       // TODO: 사칙연산 구현
-      "÷": () => {},
-      "×": () => {},
-      "-": () => {},
+      "÷": () => {
+        if (lastChar !== "" && !operatorKeys.includes(lastChar)) {
+          this.setState({ displayValue: displayValue + "÷" });
+        }
+      },
+      "×": () => {
+        if (lastChar !== "" && !operatorKeys.includes(lastChar)) {
+          this.setState({ displayValue: displayValue + "×" });
+        }
+      },
+      "-": () => {
+        if (lastChar !== "" && !operatorKeys.includes(lastChar)) {
+          this.setState({ displayValue: displayValue + "-" });
+        }
+      },
       "+": () => {
         // + 연산 참고하여 연산 구현
         if (lastChar !== "" && !operatorKeys.includes(lastChar)) {
@@ -70,17 +97,24 @@ class Calculator extends React.Component {
         if (lastChar !== "" && operatorKeys.includes(lastChar)) {
           displayValue = displayValue.substr(0, displayValue.length - 1);
         } else if (lastChar !== "") {
+          let prevVal = displayValue;
           displayValue = evalFunc(displayValue);
+          this.setState({ history: history.concat({ prevVal, displayValue }) });
         }
         this.setState({ displayValue });
       },
-      ".": () => {},
+      ".": () => {
+        if (Number(displayValue) !== 0) {
+          displayValue += ".";
+          this.setState({ displayValue });
+        }
+      },
       "0": () => {
         if (Number(displayValue) !== 0) {
           displayValue += "0";
           this.setState({ displayValue });
         }
-      }
+      },
     };
 
     if (proc[key]) {
@@ -97,11 +131,14 @@ class Calculator extends React.Component {
         <Panel>
           <Display displayValue={this.state.displayValue} />
           <ButtonGroup onClickButton={this.onClickButton}>
-            <Button size={2} color="gray">
+            <Button size={1} color="gray">
               AC
             </Button>
             <Button size={1} color="gray">
               BS
+            </Button>
+            <Button size={1} color="gray">
+              √
             </Button>
             <Button size={1} color="gray">
               ÷
@@ -135,8 +172,22 @@ class Calculator extends React.Component {
             </Button>
           </ButtonGroup>
         </Panel>
-        {/* TODO: History componet를 이용해 map 함수와 Box styled div를 이용해 history 표시 */}
-
+        <History>
+          {this.state.history.map((x, i) => {
+            return (
+              <Box
+                key={i}
+                onClick={() => {
+                  this.setState({ displayValue: x.prevVal });
+                }}
+              >
+                {x.prevVal}
+                <br />= {x.displayValue}
+              </Box>
+            );
+          })}
+        </History>
+        {/* TODO: History component를 이용해 map 함수와 Box styled div를 이용해 history 표시 */}
       </Container>
     );
   }
